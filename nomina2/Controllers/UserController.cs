ca957400fac477e8be0674c0d0fa3b20d882bf08
MICHAEL DAVID MORALES CUBILLO <mmorales16@gmail.com>
2023-08-04 23:43:55 -0600

    Continuación de nomina

---
+++ nomina2/Controllers/UserController.cs
@@ -0,0 +1,175 @@
+﻿
+using nomina2.Models.DAO;
+using nomina2.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nomina2.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserDAO userRepository = new UserDAO();
+        // GET: User
+        public ActionResult Index(string searchKeyword)
+        {
+            // Devuelve la vista Index con la lista de usuarios
+            return View(userRepository.ReadUsers(searchKeyword));
+        }
+
+        public ActionResult RolesList()
+        {
+            // Devuelve la vista Index con la lista de usuarios
+            return View(userRepository.ReadRoles());
+        }
+
+        public ActionResult CreateUser()
+        {
+            // Obtener los roles disponibles
+            var roles = userRepository.ReadRoles().ToList();
+            ViewBag.Roles = new SelectList(roles, "id", "Description");
+            return View();
+        }
+
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult CreateUser(UserDTO user)
+        {
+            try
+            {
+                string result = userRepository.InsertUser(user);
+
+                if (result == "Success")
+                {
+                    // Redireccionar a la vista "Index" en caso de éxito
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Si la inserción falla, agregar un mensaje de alerta a la ViewBag
+                    ViewBag.ErrorMessage = "Error al insertar el usuario en la base de datos.";
+                }
+            }
+            catch (Exception ex)
+            {
+                // En caso de excepción, agregar un mensaje de alerta a la ViewBag
+                ViewBag.ErrorMessage = "Ocurrió un error durante la inserción del usuario: " + ex.Message;
+            }
+
+            // Devolver la vista "CreateUser" con los datos del usuario ingresados previamente
+            // y el mensaje de alerta (si corresponde).
+            var roles = userRepository.ReadRoles().ToList();
+            ViewBag.Roles = new SelectList(roles, "id", "Description");
+            return View(user);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            // Muestra la vista de inicio de sesión
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserDTO user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserDAO userDAO = new UserDAO();
+                bool isValidCredentials = userDAO.ValidateUser(user.Email, user.Password);
+
+                if (isValidCredentials)
+                {
+                    // Credenciales válidas, permite el acceso (por ejemplo, redirige a la página de inicio)
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Credenciales inválidas, muestra mensaje de error
+                    ViewBag.ErrorMessage = "Incorrect credentials, check the email and password";
+                }
+            }
+
+            // Si el modelo no es válido o las credenciales son inválidas, vuelve a mostrar la vista de inicio de sesión con los errores
+            return View(user);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                // Intenta obtener un usuario específico utilizando el método GetUserById del repositorio UserDAO
+                UserDTO user = userRepository.GetUserById(id);
+                if (user != null)
+                {
+                    // Si el usuario existe, muestra la vista de edición con los detalles del usuario
+                    return View(user);
+                }
+                else
+                {
+                    Console.WriteLine("User not found");
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error getting user: " + ex.Message);
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: User/Edit/
+        [HttpPost]
+        public ActionResult Edit(UserDTO user)
+        {
+            try
+            {
+                // Intenta actualizar los detalles del usuario utilizando el método UpdateUser del repositorio UserDAO
+                string result = userRepository.UpdateUser(user);
+                Console.WriteLine("User updated: " + result);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error updating user: " + ex.Message);
+                return View(user);
+            }
+        }
+        // GET: User/Delete/
+        public ActionResult Delete(int id)
+        {
+            // Obtiene un usuario específico utilizando el método GetUserById del repositorio UserDAO
+            UserDTO user = userRepository.GetUserById(id);
+            if (user == null)
+            {
+                // El usuario no existe, mostrar mensaje de error o redirigir a otra vista
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+        // POST: User/Delete/
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                // Intenta eliminar el usuario utilizando el método DeleteUser del repositorio UserDAO
+                string result = userRepository.DeleteUser(id);
+                Console.WriteLine("User deleted: " + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error deleting user: " + ex.Message);
+            }
+            // Redirige a la vista Index después de eliminar el usuario
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ nomina2/Models/DAO/UserDAO.cs
@@ -0,0 +1,286 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using nomina2.Models.DTO;
+using nomina2.Models;
+
+namespace nomina2.Models.DAO
+{
+    public class UserDAO
+    {
+        public List<UserDTO> ReadUsers(string searchKeyword)
+        {
+            List<UserDTO> users = new List<UserDTO>();
+
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT * FROM tb_users WHERE name LIKE @searchKeyword OR email LIKE @searchKeyword";
+
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@searchKeyword", "%" + searchKeyword + "%");
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                UserDTO user = new UserDTO();
+                                user.Id = reader.GetInt32("id");
+                                user.Name = reader.GetString("name");
+                                user.Last_Name = reader.GetString("last_name");
+                                user.Email = reader.GetString("email");
+                                user.Telephone_number = reader.GetString("telephone_number");
+                                user.Department_id = reader.GetInt32("department_id");
+                                user.Password = reader.GetString("password");
+                                user.Type_payment = reader.GetString("type_payment");
+                                user.Amount_salary = reader.GetDecimal("amount_salary");
+                                user.Role_id = reader.GetInt32("role_id");
+                                user.State = reader.GetString("state");
+                                user.Update_date = reader.GetDateTime("update_date");
+                                user.Update_user = reader.GetString("update_user");
+                                user.Create_date = reader.GetDateTime("create_date");
+                                user.Create_user = reader.GetString("create_user");
+                                users.Add(user);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in nomina.Models.DAO.UserDAO.ReadUsers:" + ex.Message);
+            }
+            return users;
+        }
+
+
+        public List<Roles> ReadRoles()
+        {
+            List<Roles> roles = new List<Roles>();
+
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT * FROM tb_roles";
+
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Roles role = new Roles();
+                                role.Id = reader.GetInt32("id");
+                                role.Description = reader.GetString("description");
+
+                                roles.Add(role);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in nomina.Models.DAO.Roles.InserUser:" + ex.Message);
+            }
+            return roles;
+        }
+
+
+        public string InsertUser(UserDTO user)
+
+
+        {
+            string response = "Failed";
+
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string insertUserQuery = "INSERT INTO tb_users (name, last_name, email, telephone_number, department_id, password, type_payment, amount_salary, role_id, state, update_date, update_user, create_date, create_user ) VALUES (@name, @last_name, @email, @telephone_number, @department_id, @password, @type_payment, @amount_salary, @role_id, @state, @update_date, @update_user, @create_date, @create_user)";
+
+
+                    using (MySqlCommand userCommand = new MySqlCommand(insertUserQuery, connection))
+                    {
+                        userCommand.Parameters.AddWithValue("@name", user.Name);
+                        userCommand.Parameters.AddWithValue("@last_name", user.Last_Name);
+                        userCommand.Parameters.AddWithValue("@email", user.Email);
+                        userCommand.Parameters.AddWithValue("@telephone_number", user.Telephone_number);
+                        userCommand.Parameters.AddWithValue("@department_id", user.Department_id);
+                        userCommand.Parameters.AddWithValue("@password", user.Password);
+                        userCommand.Parameters.AddWithValue("@type_payment", string.IsNullOrEmpty(user.Type_payment) ? "" : user.Type_payment);
+                        userCommand.Parameters.AddWithValue("@amount_salary", user.Amount_salary);
+                        userCommand.Parameters.AddWithValue("@role_id", user.Role_id);
+                        // Asignar el valor fijo "active" al parámetro @state
+                        userCommand.Parameters.AddWithValue("@state", "active");
+                        userCommand.Parameters.AddWithValue("@update_date", user.Update_date);
+                        userCommand.Parameters.AddWithValue("@update_user", string.IsNullOrEmpty(user.Update_user) ? "MMORALES" : user.Update_user);
+                        // Asignar automáticamente la fecha y hora actual al parámetro @create_date
+                        userCommand.Parameters.AddWithValue("@create_date", DateTime.Now);
+                        userCommand.Parameters.AddWithValue("@create_user", string.IsNullOrEmpty(user.Create_user) ? "ADMIN" : user.Create_user);
+
+                        int rowsAffected = userCommand.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            response = "Success";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in UserDAO.InsertUser: " + ex.Message);
+            }
+
+            return response;
+        }
+
+
+        public bool ValidateUser(string email, string password)
+        {
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT COUNT(*) FROM tb_users WHERE email = @Email AND password = @Password";
+
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+                        return count > 0; // Si count es mayor a 0, las credenciales son válidas.
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in nomina.Models.DAO.UserDAO.ValidateUser: " + ex.Message);
+                return false; // En caso de error, consideramos las credenciales inválidas.
+            }
+        }
+
+
+        //
+        public UserDTO GetUserById(int id)
+        {
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string selectQuery = "SELECT Id, Name, Email FROM tb_users WHERE Id = @id";
+
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                UserDTO user = new UserDTO
+                                {
+                                    Id = Convert.ToInt32(reader["Id"]),
+                                    Name = reader["Name"].ToString(),
+                                    Email = reader["Email"].ToString()
+                                };
+
+                                return user;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in UserDAO.GetUserById: " + ex.Message);
+            }
+
+            return null;
+        }
+
+        public string UpdateUser(UserDTO user)
+        {
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string updateQuery = "UPDATE tb_users SET name = @name, email = @email WHERE Id = @id";
+
+                    using (MySqlCommand command = new MySqlCommand(updateQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", user.Name);
+                        command.Parameters.AddWithValue("@email", user.Email);
+                        command.Parameters.AddWithValue("@id", user.Id);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            return "Success";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in UserDAO.UpdateUser: " + ex.Message);
+            }
+
+            return "Failed";
+        }
+
+        public string DeleteUser(int id)
+        {
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string deleteQuery = "DELETE FROM tb_users WHERE Id = @id";
+
+                    using (MySqlCommand command = new MySqlCommand(deleteQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            return "Success";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in UserDAO.DeleteUser: " + ex.Message);
+            }
+
+            return "Failed";
+        }
+
+
+
+
+    }
+}
